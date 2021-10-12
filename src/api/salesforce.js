5441705e3578c2c6cd274d111413a90edbbaa7c3
Jorge Barrios <jorge@lobstermarketing.com>
2021-10-12 11:22:47 -0500

    api test

---
+++ src/api/salesforce.js
@@ -0,0 +1,90 @@
+var axios = require("axios").default;
+const jsforce = require('jsforce');
+//0053F000007Km5mQAC - jbarrios-test@lobstermarketing.com.partial
+//0053F0000078zrBQAQ - jorge@lobstermarketing.com.partial
+///005f4000003pHIJ - austin@pcocentral.com.partial
+exports.handler = async (event, context) => {
+    let randomNumb =  Math.floor(100000 + Math.random() * 900000);
+    let tasks = null;
+    let project_name = null;
+    let account = null;
+    const today = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toJSON().slice(0,10)
+    let auth = await axios.post(`${process.env.ACCESS_TOKEN_URL}?grant_type=password&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET_ID}&username=${process.env.USERNAME}&password=${process.env.PASSWORD}${process.env.SECURITY_TOKEN}`)
+    
+    tasks =  JSON.parse(event.body);
+    project_name = tasks.project_name;
+    account = tasks.account;
+    tasks = tasks.tasks;
+    let payloadTasks = [];
+    
+    tasks.map(task=>{
+        console.log(task)
+        if(Object.keys(task.urls).length !== 0){            
+            let description = `${task.desc.toUpperCase()} \n\n`;
+            description += `How to fix it: \n\n`
+            description += `${task.howtofix.replace(/(<([^>]+)>)/gi, "")} \n\n`
+            description += `Affected Pages: \n\n`
+            for (const [title, urls] of Object.entries(task.urls)) {
+                description += `- ${title} \n`
+                urls.map(url=>{ description += `-- ${url} \n`});
+                description += `\n`;
+            }
+        
+            
+            
+                
+            payloadTasks.push(  
+                {
+                    Subject: "#"+ randomNumb +" Semrush - " + project_name +" - "+ task.desc ,
+                    Priority: 'Medium',
+                    Description: description,
+                    ActivityDate: today,
+                    OwnerId: task.notify,
+                    WhatId: account,
+                    Team__c: "Support",
+                    Task_Type__c: "Site Audit",
+                    Task_Category__c: "Quality Check",
+                    Recurring_Task__c: false,
+                }
+            );
+
+        }
+        
+        
+    })
+    console.log("payload # ",payloadTasks.length)
+
+    let r = await createTasks(payloadTasks, auth);
+    payloadTasks = [];
+    //console.log("Promise All response", r.data);
+
+    return {
+        statusCode:200,
+        //body:JSON.stringify(r.data);
+        body: "ok"
+    }
+}
+
+async function  createTasks (tasks, auth){
+    let responsePromiseAll = null;
+    let promises = [];
+    tasks.map(payload=>{
+        promises.push(
+        axios({ 
+            method: 'POST',
+            url: `${process.env.SF_URL}/services/data/v49.0/sobjects/Task/`,
+                headers: {
+                'content-type': 'application/json',
+                authorization: 'Bearer ' + auth.data.access_token
+                },
+                data: payload
+            }) 
+        )
+    });
+    console.log("total promises ", promises.length)
+    responsePromiseAll = await Promise.all(promises);
+
+    return responsePromiseAll;
+    
+       
+}
